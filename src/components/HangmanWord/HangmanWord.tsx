f78e8ac487f69d035e51b9181cf8fec112de27a4
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-07-05 23:10:29 +0200

    HangmanWord.tsx added

--- src/components/HangmanWord/HangmanWord.tsx
+++ src/components/HangmanWord/HangmanWord.tsx
@@ -18,7 +18,6 @@ const HangmanWord = () => {
                     >
                         {letter}
                     </HiddenLetter>
-
                 </Letter>
             ))}
         </Container>
