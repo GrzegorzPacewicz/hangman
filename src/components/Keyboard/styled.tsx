d8afd4c24ef7f6de67ecf0d1ac1d42fbd9be1a80
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-07-10 14:18:01 +0200

    Material UI installed

--- src/components/Keyboard/styled.tsx
+++ src/components/Keyboard/styled.tsx
@@ -6,28 +6,3 @@ export const Container = styled.div`
   gap: .5rem;
   align-items: stretch;
 `;
-
-export const KeyButton = styled.button`
-  width: 100%;
-  aspect-ratio: 1/1;
-  border: 3px solid black;
-  background: none;
-  font-size: 2.5rem;
-  font-weight: bold;
-  text-transform: uppercase;
-  padding: .5rem;
-  cursor: pointer;
-  color: black;
-  
-  &:hover:not(:disabled), 
-  &:focus:not(:disabled) {
-    background-color: hsl(200, 100%, 75%);    
-  }
-  .active {
-    background-color: hsl(200, 100%, 50%);
-    color: white;
-  }
-    .inactive {
-    opacity: .3;
-  }
-`;
