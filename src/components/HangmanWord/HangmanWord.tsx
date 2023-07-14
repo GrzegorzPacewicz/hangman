2344aa8436c940cce5eec7ecdc46ec80f5171e61
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-07-14 10:22:38 +0200

    Black version of the Game

--- src/components/HangmanWord/HangmanWord.tsx
+++ src/components/HangmanWord/HangmanWord.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, HiddenLetter, Letter} from "./styled";
+import {Container, Letter} from "./styled";
 
 type HangmanWordProps = {
     guessedLetters: string[]
